1a4a52c8f2ba09f31eebfe81f0ee06bc3e0f0b63
Zhu Zhong <zhongzhu@ymail.com>
2015-08-22 22:33:18 +0800

    no need to check in plugins

--- cordova/platforms/android/assets/www/cordova_plugins.js
+++ cordova/platforms/android/assets/www/cordova_plugins.js
@@ -4,28 +4,12 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
-    },
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    },
-    {
-        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
-        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
-        "clobbers": [
-            "cordova.plugins.barcodeScanner"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0",
-    "org.apache.cordova.inappbrowser": "0.6.0",
-    "phonegap-plugin-barcodescanner": "4.0.1"
+    "cordova-plugin-whitelist": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
